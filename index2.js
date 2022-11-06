9e3302e86e9b0378d7181af35ca154f61aaa7325
Pablo Barrio <barriogarcia.pablo@gmail.com>
2022-11-06 09:10:21 +0100

    agregados 2 y 3

---
+++ index2.js
@@ -0,0 +1,18 @@
+let nombre = 'Pablo';
+let edad = 40;
+let mayorDeEdad = true;
+
+let direccion = {
+    calle: 'La garoja',
+    numero: 20,
+    piso: 2,
+    letra: 'A'
+};
+
+let coloresFavoritos = ['azul', 'verde', 'morado', 'naranja'];
+let lenguajesFavoritos = ['javascript', 'php', 'cobol'];
+
+let mejorLenguaje = lenguajesFavoritos[0];
+let peorLenguaje = lenguajesFavoritos[lenguajesFavoritos.length - 1];
+
+console.log(peorLenguaje);
